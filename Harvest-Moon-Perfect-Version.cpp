fb57d368380fd5687413f2a8232e4eb014527a1d
juniornff <33964836+juniornff@users.noreply.github.com>
2018-07-08 02:49:20 -0400

    Principio del gotoxy y primera compilacion
    Se escribio la estructura basica del Gotoxy y se probo por primera vez

--- Harvest-Moon-Perfect-Version.cpp
+++ Harvest-Moon-Perfect-Version.cpp
@@ -5,5 +5,15 @@
 
 int main() {
 	printf("*");
+		
+	HANDLE hCon;
+	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD dwPos;
+	dwPos.X = 4;
+	dwPos.Y = 5;
+	SetConsoleCursorPosition(hCon, dwPos);
+	
+	printf("*");
+	
 	return 0;
 }
