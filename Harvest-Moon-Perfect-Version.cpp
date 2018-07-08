5272cdfdbe48674aac62b4ff5e893dbce3e79d3a
juniornff <33964836+juniornff@users.noreply.github.com>
2018-07-08 03:27:10 -0400

    Mover con flechas y ocultar la flechita de cursor
    Hicimos la forma de mover un punto con las flechas (aun no la hacemos funcion, Hicimos la funcion para orcultarcursor

--- Harvest-Moon-Perfect-Version.cpp
+++ Harvest-Moon-Perfect-Version.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <windows.h>
+#include <conio.h>
+
+#define ARRIBA 72
+#define IZQUIERDA 75
+#define DERECHA 77
+#define ABAJO 80
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -12,8 +18,33 @@ void gotoxy(int x, int y){
 	SetConsoleCursorPosition(hCon, dwPos);
 }
 
+void OcultarCursor(){
+	HANDLE hCon;
+	hCon = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cci;
+	cci.bVisible = FALSE;
+	SetConsoleCursorInfo(hCon,&cci);
+}
+
 int main() {
-	gotoxy(15,10);
-	printf("*");
+	OcultarCursor();
+	int x = 10,y = 10;
+	gotoxy(x,y); printf("*");
+	bool game_over = false;
+
+	while(!game_over){
+		
+		if(kbhit()){
+			char tecla = getch();
+			gotoxy(x,y); printf(" ");
+			if(tecla == IZQUIERDA){x--;}//izquierda
+			if(tecla == DERECHA){x++;}//derecha
+			if(tecla == ARRIBA){y--;}//arriba
+			if(tecla == ABAJO){y++;}//abajo
+			gotoxy(x,y); printf("*");
+		}
+		
+		Sleep(30);
+	}
 	return 0;
 }
