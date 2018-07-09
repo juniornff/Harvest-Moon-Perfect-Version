2e70461b6e4b55507381ddce8e16e93dee452bfc
juniornff <33964836+juniornff@users.noreply.github.com>
2018-07-09 12:14:56 -0400

    Modificacion del estilo de las casa y funcion pintar campo
    Se hizo mas detalle en la estructura de la casas (aun no en todas) y se creo la funcion para intar campo y se esta averiguando como pintar zonas especificas

--- Pruebas Pintar.cpp
+++ Pruebas Pintar.cpp
@@ -8,20 +8,39 @@ void SetColor (unsigned short color){
 }
 int main()
 {
-    SetColor(4);printf("%c%c%c%c%c%c%c%c%c%c\n",177,177,177,177,177,177,177,177,177,177);
-    SetColor(14);printf("%c%c%c%c%c%c%c%c%c%c\n",178,178,178,178,178,178,178,178,178,178);
-    SetColor(14);printf("%c",178);SetColor(118);printf("[|]");SetColor(14);printf("%c%c%c%c%c%c\n",178,178,178,178,178,178);
-    SetColor(15);printf("Granero\n\n");
+	SetColor(15);printf(" ");SetColor(196);printf("%c%c%c%c%c",177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
+    SetColor(196);printf("%c%c%c%c%c%c%c",177,177,177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
+    SetColor(228);printf("%c",186);SetColor(230);printf("%c%c%c",178,178,178);SetColor(15);printf("%c",206);SetColor(230);printf("%c",178);SetColor(228);printf("%c",186);SetColor(230);printf("%c%c\n",178,178);
+    SetColor(228);printf("%c",186);SetColor(230);printf("%c",178);SetColor(118);printf("[]");SetColor(230);printf("%c%c",178,178);SetColor(228);printf("%c",186);SetColor(230);printf("%c%c\n",178,178);
+	SetColor(15);printf("Casa\n\n");
     
-	SetColor(4);printf("%c%c%c%c%c%c\n",177,177,177,177,177,177);
-    SetColor(14);printf("%c%c%c%c%c%c\n",178,178,178,178,178,178);
-    SetColor(14);printf("%c%c",178,178);SetColor(118);printf("[]");SetColor(14);printf("%c%c\n",178,178);
-    SetColor(15);printf("Gallinero/Casa/Establo\n\n");
+    SetColor(15);printf(" ");SetColor(196);printf("%c%c%c%c%c%c",177,177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
+	SetColor(196);printf("%c%c%c%c%c%c%c%c",177,177,177,177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
+    SetColor(228);printf("%c",186);SetColor(230);printf("%c%c%c%c%c%c",178,178,178,178,178,178);SetColor(228);printf("%c",186);SetColor(230);printf("%c%c\n",178,178);
+    SetColor(228);printf("%c",186);SetColor(230);printf("%c%c",178,178);SetColor(118);printf("[]");SetColor(230);printf("%c%c",178,178);SetColor(228);printf("%c",186);SetColor(230);printf("%c%c\n",178,178);
+    SetColor(15);printf("Gallinero/Establo\n\n");
     
-	SetColor(4);printf("%c%c%c%c%c%c%c%c%c%c\n",177,177,177,177,177,177,177,177,177,177);
-    SetColor(14);printf("%c%c%c%c%c%c%c%c%c%c\n",178,178,178,178,178,178,178,178,178,178);
-    SetColor(14);printf("%c%c%c",178,178,178);SetColor(118);printf("[|]");SetColor(14);printf("%c%c%c%c\n",178,178,178,178);
+	SetColor(196);printf("%c%c%c%c%c%c%c%c%c%c\n",177,177,177,177,177,177,177,177,177,177);
+    SetColor(230);printf("%c%c%c%c%c%c%c%c%c%c\n",178,178,178,178,178,178,178,178,178,178);
+    SetColor(230);printf("%c%c%c",178,178,178);SetColor(118);printf("[|]");SetColor(230);printf("%c%c%c%c\n",178,178,178,178);
     SetColor(15);printf("Casa grande/Super/Clinica\n\n");
+    
+    int i = 0, j = 0;
+    int x = 5, y = 5, c = 108, t = 64; //rosa
+    
+    for(i = 0; i <= 25; i++){
+    	for(j = 0; j <= 43; j++){
+    		if(j == x && i == y){
+    			SetColor(c);printf("%c",t);
+			}else{
+				if(j == 43){
+    				SetColor(98);printf("%c\n",32);
+				}else{
+					SetColor(98);printf("%c",32);
+				}
+			}
+		}
+	}
     
     SetColor(15);
 	system("pause");   
