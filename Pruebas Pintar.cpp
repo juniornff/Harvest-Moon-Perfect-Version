841e08f5ac9815b3e0dd1645870d2e03d759fd6a
juniornff <33964836+juniornff@users.noreply.github.com>
2018-07-10 01:28:26 -0400

    Hacer un cusomizador de apariencia para el pintar casas (en proceso)

--- Pruebas Pintar.cpp
+++ Pruebas Pintar.cpp
@@ -12,6 +12,17 @@ void SetColor (unsigned short color){
 	SetConsoleTextAttribute(hcon,color);
 }
 
+void PaletaDeColores(){
+	SetColor(25);printf("25");SetColor(15);printf("  ");SetColor(38);printf("38\n");
+	SetColor(59);printf("59");SetColor(15);printf("  ");SetColor(76);printf("76\n");
+	SetColor(93);printf("93");SetColor(15);printf("  ");SetColor(110);printf("110\n");
+	SetColor(127);printf("127");SetColor(15);printf(" ");SetColor(128);printf("128\n");
+	SetColor(145);printf("145");SetColor(15);printf(" ");SetColor(162);printf("162\n");
+	SetColor(185);printf("185");SetColor(15);printf(" ");SetColor(196);printf("196\n");
+	SetColor(213);printf("213");SetColor(15);printf(" ");SetColor(230);printf("230\n");
+	SetColor(247);printf("247\n\n");SetColor(15);
+}
+
 void PintarCasa(){
 	SetColor(15);printf(" ");SetColor(196);printf("%c%c%c%c%c",177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
     SetColor(196);printf("%c%c%c%c%c%c%c",177,177,177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
@@ -20,11 +31,11 @@ void PintarCasa(){
 	SetColor(15);printf("Casa\n\n");
 }
 
-void PintarGraneroEstablo(){
-	SetColor(15);printf(" ");SetColor(196);printf("%c%c%c%c%c%c",177,177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
-	SetColor(196);printf("%c%c%c%c%c%c%c%c",177,177,177,177,177,177,177,177);SetColor(230);printf("%c%c\n",178,178);
-    SetColor(228);printf("%c",186);SetColor(230);printf("%c%c%c%c%c%c",178,178,178,178,178,178);SetColor(228);printf("%c",186);SetColor(230);printf("%c%c\n",178,178);
-    SetColor(228);printf("%c",186);SetColor(230);printf("%c%c",178,178);SetColor(118);printf("[]");SetColor(230);printf("%c%c",178,178);SetColor(228);printf("%c",186);SetColor(230);printf("%c%c\n",178,178);
+void PintarGraneroEstablo(int techo, int pared, int puerta){
+	SetColor(15);printf(" ");SetColor(techo);printf("%c%c%c%c%c%c",177,177,177,177,177,177);SetColor(pared);printf("%c%c\n",178,178);
+	SetColor(techo);printf("%c%c%c%c%c%c%c%c",177,177,177,177,177,177,177,177);SetColor(pared);printf("%c%c\n",178,178);
+    SetColor(pared);printf("%c",186);SetColor(pared);printf("%c%c%c%c%c%c",178,178,178,178,178,178);SetColor(pared);printf("%c",186);SetColor(pared);printf("%c%c\n",178,178);
+    SetColor(pared);printf("%c",186);SetColor(pared);printf("%c%c",178,178);SetColor(puerta);printf("[]");SetColor(pared);printf("%c%c",178,178);SetColor(pared);printf("%c",186);SetColor(pared);printf("%c%c\n",178,178);
     SetColor(15);printf("Gallinero/Establo\n\n");
 }
 
@@ -55,13 +66,19 @@ void PintarCampoCultivo(){
 
 int main()
 {
+	int techo, pared, puerta;
 	PintarCasa();
-	PintarGraneroEstablo();
-	SetColor(196);printf("%c%c%c%c%c%c%c%c%c%c\n",177,177,177,177,177,177,177,177,177,177);
-    SetColor(230);printf("%c%c%c%c%c%c%c%c%c%c\n",178,178,178,178,178,178,178,178,178,178);
-    SetColor(230);printf("%c%c%c",178,178,178);SetColor(118);printf("[|]");SetColor(230);printf("%c%c%c%c\n",178,178,178,178);
-    SetColor(15);printf("Casa grande/Super/Clinica\n\n");
-    PintarCampoCultivo();
+	cout<<"Techo"<<endl;
+	PaletaDeColores();
+	cin>>techo;
+	cout<<"Pared"<<endl;
+	PaletaDeColores();
+	cin>>pared;
+	cout<<"Puerta"<<endl;
+	PaletaDeColores();
+	cin>>puerta;
+	PintarGraneroEstablo(techo,pared,puerta);
+	PintarCampoCultivo();
     SetColor(15);
 	system("pause");   
 }
