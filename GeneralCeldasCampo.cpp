68e0edde7d194276ee71c4be13b48520840924fd
juniornff <33964836+juniornff@users.noreply.github.com>
2018-07-15 01:42:06 -0400

    Logrado
    Ahora el codigo verifica si el archivo "CeldasCampo.txt" existe antes de empezar a comparar celdas.

--- GeneralCeldasCampo.cpp
+++ GeneralCeldasCampo.cpp
@@ -25,16 +25,27 @@ int main(){
 	ofstream archivo;
 	ifstream CeldasCampo;
 	int origenx[42][24], origeny[42][24], origeni[42][24], origenc[42][24], x, ItemID, Color;
+	bool origen;
 	srand (time(NULL));
 	
-	CeldasCampo.open("CeldasCampo.txt",ios::in);
-	for(int oi = 0; oi <= 24; oi++){
-    	for(int oj = 0; oj <= 42; oj++){
-			CeldasCampo>>origenx[oj][oi]>>origeny[oj][oi]>>origeni[oj][oi]>>origenc[oj][oi];
+	if(is_file("CeldasCampo.txt")){
+		CeldasCampo.open("CeldasCampo.txt",ios::in);
+		for(int oi = 0; oi <= 24; oi++){
+    		for(int oj = 0; oj <= 42; oj++){
+				CeldasCampo>>origenx[oj][oi]>>origeny[oj][oi]>>origeni[oj][oi]>>origenc[oj][oi];
+			}
 		}
+		CeldasCampo.close();
+		remove("CeldasCampo.txt");
+		origen = true;
 	}
-	CeldasCampo.close();
-	remove("CeldasCampo.txt");
+	
+	if(origen){
+		printf("Hay archivo de origen\n");
+	}else{
+		printf("No hay archivo de origen\n");
+	}
+	system("pause");
 	
 	archivo.open("CeldasCampo.txt",ios::out);
 	for(int i = 0; i <= 24; i++){
@@ -44,62 +55,82 @@ int main(){
     		switch(x){
     			case 1: ItemID = 79;
 						Color = 104;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 2: ItemID = 207;
 						Color = 100;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 3: ItemID = 42;
 						Color = 98;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 4: ItemID = 47;
 						Color = 100;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 5: ItemID = 127;
 						Color = 104;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 6: ItemID = 32;
 						Color = 102;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 7: ItemID = 32;
 						Color = 102;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 8: ItemID = 32;
 						Color = 102;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 9: ItemID = 32;
 						Color = 102;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 				case 10: ItemID = 32;
 						Color = 102;
-						if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
-							goto cambio;
+						if(origen){
+							if(j == origenx[j][i] && i == origeny[j][i] && ItemID == origeni[j][i]){
+								goto cambio;
+							}
 						}
 						break;
 			}
